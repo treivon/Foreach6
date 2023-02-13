84c4a912ee7a8d8ee3c2f4e320d4c3fe55bb3e5c
opilane <opilane@HP-CZC0359NNC>
2023-02-13 14:55:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new List<string> { "tea", "car", "fish", "house" };
+
+            words.ForEach(e => Console.WriteLine(e));
+        }
+    }
+}
